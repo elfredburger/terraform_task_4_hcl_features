2aaabdd1bffc9ad2160a5d7ac9893f0ad833f266
elfredburger <elfredfredburger@gmail.com>
2024-09-17 03:15:25 +0200

    Solution

--- network.tf
+++ network.tf
@@ -42,8 +42,6 @@ resource "azurerm_network_security_group" "name" {
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
 
-
-
   dynamic "security_rule" {
     for_each = local.network_security_rules
     content {
@@ -86,5 +84,3 @@ resource "azurerm_network_interface" "main" {
     private_ip_address_allocation = "Dynamic"
   }
 }
-
-
--- variables.tf
+++ variables.tf
@@ -1,3 +1,11 @@
 variable "prefix" {
   default = "tfvmex"
 }
+
+variable "tags" {
+  default = {
+    Environment = "Production"
+    Owner       = "DevOps"
+    Project     = "Infrastructure"
+  }
+}
